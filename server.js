84a60fb470d23540ebcaa8bc50f2da5b403a9258
Nicolas Font <nicolasfont@gmail.com>
2014-02-13 03:17:50 -0500

    Add root url handling

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express'),
     server = express.createServer();
 
+server.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
 server.use(express.static(__dirname));
 
 server.listen(process.env.PORT || process.env.C9_PORT);
