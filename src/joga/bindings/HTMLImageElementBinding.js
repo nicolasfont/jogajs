fcfd47879573847f207bc6d0bdf490c1cd526f8b
Nicolas Font <nicolasfont@gmail.com>
2014-06-01 01:11:46 +0000

    Img alt binding

--- src/joga/bindings/HTMLImageElementBinding.js
+++ src/joga/bindings/HTMLImageElementBinding.js
@@ -18,5 +18,17 @@ define(['joga/bindings/ElementBinding', 'joga/computedProperty'], function (Elem
         computed.subscribe(this.src.update);
     };
     
+    HTMLImageElementBinding.prototype.alt = function (dataExpression) {
+        var computed = computedProperty(dataExpression);
+
+        this.alt.update = function () {
+            this.element.alt = computed.apply(this.model);
+        }.bind(this);
+
+        this.alt.update();
+
+        computed.subscribe(this.alt.update);
+    };
+    
     return HTMLImageElementBinding;
 });
--- tests/bindings/HTMLImageElementBindingTests.js
+++ tests/bindings/HTMLImageElementBindingTests.js
@@ -16,4 +16,19 @@ define(['joga'], function (joga) {
         
         equal(model.element().src, "http://jogajs.test/2");
     });
+    
+    test("data-alt binding updates image alt", function() {
+        var model = new Model();
+
+        function Model() {
+            this.name = joga.stringProperty("1");
+            this.element = joga.elementProperty('<img data-alt="this.name()"/>');
+        }
+
+        equal(model.element().alt, "1");
+        
+        model.name("2");
+        
+        equal(model.element().alt, "2");
+    });
 });
